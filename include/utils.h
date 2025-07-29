fc39ac2508daad1f451619a533a025cf67dabf26
john <iamachide10@gmail.com>
2025-07-29 16:33:00 +0000

    finished utils

--- include/utils.h
+++ include/utils.h
@@ -1,1 +1,24 @@
- 
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+// Function to clear the console screen
+void clearScreen();
+
+// Function to prevent everything from moving too fast
+// This pauses the program for a specified number of milliseconds
+void delay(int milliseconds);
+
+// Function to draw a border around the console window
+void drawBorder(int width, int height);
+
+// This function prints messages centered within the given width
+void printCentered(const std::string& text, int width);
+
+// This function helps print debug messages to the console
+void logDebug(const std::string& label, const std::string& value);
+
+
+#endif // UTILS_H
+
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,49 @@
- 
+#include "utils.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+void delay(int milliseconds) {
+    this_thread::sleep_for(chrono::milliseconds(milliseconds));
+}
+
+void drawBorder(int width, int height) {
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if (y == 0 || y == height - 1 || x == 0 || x == width - 1) {
+                cout << "#";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void printCentered(const string& text, int width) {
+    int padding = (width - text.length()) / 2;
+    if (padding > 0) {
+        cout << string(padding, ' ');
+    }
+    cout << text << endl;
+}
+
+void logDebug(const string& label, const string& value) {
+    cout << "[DEBUG] " << label << ": " << value << endl;
+}
