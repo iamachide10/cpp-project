bc2242657c75d62bd224334a119fb22793f904d6
Bidu <abigailadusei17@gmail.com>
2025-07-29 15:41:49 -0700

    finished paddle.H file

--- include/paddle.h
+++ include/paddle.h
@@ -1,1 +1,29 @@
- 
+#ifndef PADDLE_H
+#define PADDLE_H
+
+class Paddle{
+private:
+// Paddle position (x is fixed, y is vertical)
+    int x, y;
+// Paddle height
+    int height;
+//Total Height of the game area
+    int screenHeight;
+
+public:
+  Paddle(int startX, int startY, int height, int screenHeight);
+
+  void moveup();
+  void movedown();
+  void draw() const;
+  int  getX()  const;
+  int  getY()  const;
+  int  getHeight() const;
+
+};
+
+#endif
+
+
+
+
