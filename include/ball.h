ca059ada3ebcbacdb23fce66a2d8af024479851f
everyone-70 <canikissyoueinstein@gmail.com>
2025-08-01 11:01:27 +0000

    finished ball.cpp and ball.h

--- include/ball.h
+++ include/ball.h
@@ -1,1 +1,37 @@
- 
+#ifndef BALL_H
+#define BALL_H
+
+class Ball {
+
+private:
+    //current position
+int x, y;
+    //direction of movement
+int width, height;
+    //game field dimention
+int width, height;
+
+public:
+    //constuctor
+Ball(int width, int height);
+     //reset the ball to center
+void reset();
+      //move the ball
+void update();
+     //bounce off top/bottom
+void bounceVertical();
+     //bounce off paddles
+void bounceHorizontal();
+
+   //check if the ball is out left
+bool isOutLeft() const;
+     //check if the ball is out right
+bool isOutRight() const;
+
+       //get current x
+int getX() const;
+       //get current y
+int getY() const;
+
+}
+#endif
--- src/ball.cpp
+++ src/ball.cpp
@@ -1,1 +1,53 @@
-  
+#include "ball.h"
+// for rand()
+#include <cstdlib>
+     //setting the size of the field
+Ball::Ball(int w, int h) : width(w), height(h) {
+    reset();
+}
+
+void Ball::reset() {
+    x = width / 2;
+    y = height / 2;
+
+    // Random direction
+    dx = (rand() % 2 == 0) ? 1 : -1;
+    dy = (rand() % 2 == 0) ? 1 : -1;
+}
+     //direction of movement
+void Ball::update() {
+    x += dx;
+    y += dy;
+
+    // Bounce off top or bottom wall
+    if (y <= 0 || y >= height - 1) {
+        bounceVertical();
+    }
+}
+     //calling the ball to bounce vertically
+void Ball::bounceVertical() {
+    dy = -dy;
+}
+     //calling the ball to bounce horizontally
+void Ball::bounceHorizontal() {
+    dx = -dx;
+}
+     //checking to see if the ball is out the left
+bool Ball::isOutLeft() const {
+    return x <= 0;
+}
+    //checking to see if the ball is out right
+bool Ball::isOutRight() const {
+    return x >= width - 1;
+}
+    //get current position of the ball x
+int Ball::getX() const {
+    return x;
+}
+    // get current position of the ball y
+int Ball::getY() const {
+    return y;
+}
+
+
+
